publify/publify

Disable STI for Sidebar

spec/lib/authors_sidebar_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe AuthorsSidebar do
-  let(:sidebar) { AuthorsSidebar.new }
+  let(:configuration) { AuthorsSidebar.new({}) }
 
   it 'is included in the list of available sidebars' do
     expect(SidebarRegistry.available_sidebars).to include(AuthorsSidebar)
@@ -11,7 +11,7 @@ describe AuthorsSidebar do
     let!(:authors) { create_list :user, 2 }
 
     it 'returns a list of users' do
-      expect(sidebar.authors).to match_array authors
+      expect(configuration.authors).to match_array authors
     end
   end
 end

project-kotinos/publify___publify

spec/models/authors_sidebar_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 describe AuthorsSidebar do
-  let(:sidebar) { described_class.new }
+  let(:configuration) { described_class.new({}) }
 
   it "is included in the list of available sidebars" do
     expect(SidebarRegistry.available_sidebars).to include(described_class)
@@ -13,7 +13,7 @@ describe AuthorsSidebar do
     let!(:authors) { create_list :user, 2 }
 
     it "returns a list of users" do
-      expect(sidebar.authors).to match_array authors
+      expect(configuration.authors).to match_array authors
     end
   end
 end